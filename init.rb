6eb493f13c090804aaa219fe4317aac8089ed2fb
chrisroberts <chrisroberts@colortechnology.com>
2011-02-18 07:52:41 -0800

    Add controller helpers

--- init.rb
+++ init.rb
@@ -1,8 +1,10 @@
 require 'jqgrid_rails/jqgrid_rails_view'
+require 'jqgrid_rails/jqgrid_rails_controller'
 require 'jqgrid_rails/jqgrid_rails_generators'
 
-# Load everything into rails
+#Load everything into rails
 if(defined? Rails)
-  ActionView::Base.send :include, JqGridView
-  ActionView::Helpers::PrototypeHelper::JavaScriptGenerator::GeneratorMethods.send :include, JqGridGenerators
+  ActionView::Base.send :include, JqGridRails::View
+  ActionController::Base.send :include, JqGridRails::Controller
+  ActionView::Helpers::PrototypeHelper::JavaScriptGenerator::GeneratorMethods.send :include, JqGridRails::Generators
 end
