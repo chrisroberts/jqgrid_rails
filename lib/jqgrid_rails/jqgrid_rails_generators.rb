c509e3fce7f3ab5d022f13a6d60f030b5ddd8c88
chrisroberts <chrisroberts@colortechnology.com>
2011-02-19 07:21:45 -0800

    Split up grid layout and initialization in view. Provide JS generator to produce grids for xhr requests

--- lib/jqgrid_rails/jqgrid_rails_generators.rb
+++ lib/jqgrid_rails/jqgrid_rails_generators.rb
@@ -1,4 +1,9 @@
 module JqGridRails
   module Generators
+
+    def jqgrid(grid)
+      self << grid.build
+    end
+
   end 
 end
--- lib/jqgrid_rails/jqgrid_rails_view.rb
+++ lib/jqgrid_rails/jqgrid_rails_view.rb
@@ -4,11 +4,19 @@ module JqGridRails
   module View
     include ActionView::Helpers::JavaScriptHelper
     include JqGridRails::JavascriptHelper
+
+    # grid:: JqGrid Object
+    # Writes required HTML to page for grid
+    def jqgrid_html(grid)
+      output = "<table id=\"#{grid.table_id}\"></table>"
+      output << "<div id=\"#{grid.options[:pager]}\"></div>" if grid.has_pager?
+      output.html_safe
+    end
+
     # grid:: JqGrid Object
     # Write jqGrid instructions out to page
     def jqgrid(grid)
-      output = "<table id=\"#{grid.table_id}\"></table>"
-      output << "<div id=\"#{grid.options[:pager]}\"></div>" if grid.has_pager?
+      output = jqgrid_html(grid)
       output << javascript_tag(grid.build)
       output.html_safe
     end
