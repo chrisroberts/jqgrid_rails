7f51d54319a8864676c1456a27bae316969692f1
chrisroberts <chrisroberts@colortechnology.com>
2011-04-04 13:07:12 -0700

    Only load railtie if we are in rails 3

--- lib/jqgrid_rails.rb
+++ lib/jqgrid_rails.rb
@@ -1,5 +1,13 @@
 require 'jqgrid_rails/version'
 
 if(defined?(Rails))
-  require 'jqgrid_rails/railtie'
+  if(Rails.version.split('.').first == '3')
+    require 'jqgrid_rails/railtie'
+  else
+    Dir.glob(File.join(File.dirname(__FILE__), 'jqgrid_rails', '*.rb')).each do |file|
+      unless(%w(railtie.rb tasks.rb version.rb).find{|skip| file.ends_with?(skip)})
+        require file
+      end
+    end
+  end
 end
