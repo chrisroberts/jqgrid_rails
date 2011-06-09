618498d65aa569d8f582bef9501a479e3d21e832
Chris Roberts <chrisroberts.code@gmail.com>
2011-06-09 12:38:59 -0700

    Reload the table after griding just to be safe on sorting

--- lib/jqgrid_rails/jqgrid_rails_view.rb
+++ lib/jqgrid_rails/jqgrid_rails_view.rb
@@ -37,7 +37,7 @@ module JqGridRails
       [:ondbl_click_row,:on_cell_select].each do |key|
         map_click(key, options)
       end
-      javascript_tag("tableToGrid(\"#{convert_dom_id(dom_id)}\", #{format_type_to_js(options)});")
+      javascript_tag("tableToGrid(\"#{convert_dom_id(dom_id)}\", #{format_type_to_js(options)}); jQuery(\"#{convert_dom_id(dom_id)}\").trigger('reloadGrid');")
     end
   end 
 end
