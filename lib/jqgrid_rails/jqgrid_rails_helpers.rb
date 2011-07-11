d3beaa211e94eafd15892645aa92670964bed3cb
Chris Roberts <chrisroberts.code@gmail.com>
2011-07-11 10:31:44 -0700

    Use numerical value for ID replacement to allow proper path generation when \d+ restriction is being applied via routing setup

--- lib/jqgrid_rails/jqgrid_rails_helpers.rb
+++ lib/jqgrid_rails/jqgrid_rails_helpers.rb
@@ -9,14 +9,15 @@ module JqGridRails
     # key:: ondbl_click_row/on_select_row
     # Sets up click event functions based on hash values
     def map_click(key, options)
+      id_replacement = '000'
       if(options[key].is_a?(Hash))
         @url_gen ||= JqGridRails::UrlGenerator.new
         args = options[key][:args].to_a
-        args << '!!'
+        args << id_replacement
         if(options[key][:remote])
-          options[key] = "function(id){ jQuery.get('#{@url_gen.send(options[key][:url], *args)}'.replace('!!', id)) + '#{options[key][:suffix]}'; }"
+          options[key] = "function(id){ jQuery.get('#{@url_gen.send(options[key][:url], *args)}'.replace('000', id)) + '#{options[key][:suffix]}'; }"
         else
-          options[key] = "function(id){ window.location = '#{@url_gen.send(options[key][:url], *args)}'.replace('!!', id) + '#{options[key][:suffix]}'; }"
+          options[key] = "function(id){ window.location = '#{@url_gen.send(options[key][:url], *args)}'.replace('000', id) + '#{options[key][:suffix]}'; }"
         end
       end
     end
