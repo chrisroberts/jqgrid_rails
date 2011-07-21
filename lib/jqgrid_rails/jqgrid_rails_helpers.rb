01b3077e368922f0b0c7e14c83f642256611a28b
Chris Roberts <chrisroberts.code@gmail.com>
2011-07-21 14:15:08 -0700

    Toolbar button build moved to helper module

--- lib/jqgrid_rails/jqgrid_rails_helpers.rb
+++ lib/jqgrid_rails/jqgrid_rails_helpers.rb
@@ -18,14 +18,74 @@ module JqGridRails
         args = options[key][:args].to_a
         args << id_replacement
         if(options[key][:remote])
-          options[key] = "function(id){ jQuery.ajax('#{@url_gen.send(options[key][:url], *args)}'.replace('000', id) + '#{options[key][:suffix]}', #{format_type_to_js(ajax_args)}); }"
+          options[key] = "function(id){ jQuery.ajax('#{@url_gen.send(options[key][:url], *args)}'.replace('#{id_replacement}', id) + '#{options[key][:suffix]}', #{format_type_to_js(ajax_args)}); }"
         else
           options[key] = "function(id){
-            jQuery('body').append('<form id=\"redirector\" action=\"#{@url_gen.send(options[key][:url], *args)}\" method=\"#{ajax_args[:type].upcase}\"></form>'.replace('000', id));
+            jQuery('body').append('<form id=\"redirector\" action=\"#{@url_gen.send(options[key][:url], *args)}\" method=\"#{ajax_args[:type].upcase}\"></form>'.replace('#{id_replacement}', id));
             jQuery('#redirector').submit();
           }"
         end
       end
+    end
+
+    def build_toolbar_button(url_hash)
+      id_replacement = '000'
+      url_hash[:empty_selection] ||= !url_hash[:single]
+      url_hash[:method] = url_hash[:method].to_s if url_hash[:method]
+      ajax_args = url_hash.delete(:ajax) || {}
+      ajax_args[:type] ||= url_hash.delete(:method) || 'get'
+      ajax_args[:type] = ajax_args[:type].to_s
+      classes = ['grid_toolbar_item', 'button']
+      if(url_hash[:url].is_a?(Symbol))
+        @url_gen ||= JqGridRails::UrlGenerator.new
+        args = url_hash[:args].to_a
+        args.push(id_replacement) #unless url_hash[:empty_selection]
+        url_hash[:url] = @url_gen.send(url_hash[:url], *args)
+      end
+      s = <<-EOS
+jQuery('<div class="#{(classes + url_hash[:class].to_a).compact.join(' ')}" />')
+  .text('#{escape_javascript(url_hash[:name])}')
+    .click(
+      function(){
+        var ary = jQuery('##{@table_id}').jqGrid('getGridParam', 'selarrrow');
+        if(!ary.length)
+          ary = new Array();
+        ary.push(jQuery('##{@table_id}').jqGrid('getGridParam', 'selrow'));
+        ary = ary.filter(function(elm){ return elm != null && elm.length });
+        if(!ary.length && #{format_type_to_js(url_hash[:empty_selection])}){
+          alert('Please select items from table first.');
+        } else {
+          if(!ary.length){
+            #{
+              if(url_hash[:remote])
+                "jQuery.ajax('#{url_hash[:url]}', #{format_type_to_js(ajax_args)});"
+              else
+                "jQuery('body').append('<form id=\"redirector\" action=\"#{url_hash[:url]}\" method=\"#{(url_hash[:method] || 'get').to_s.upcase}\"></form>'); jQuery('#redirector').submit();"
+              end
+            }
+          }
+          else{
+            ary_hash = {'ids[]' : ary};
+            #{
+              if(url_hash[:remote])
+                ajax_args[:data] = RawJS.new('ary_hash')
+                "jQuery.ajax('#{url_hash[:url]}'.replace('#{id_replacement}', ary[0]), #{format_type_to_js(ajax_args)});"
+              else
+                "
+                  parts = ary.map(
+                    function(item){
+                      return '<input type=\"hidden\" name=\"ids[]\" value=\"'+item+'\"/>';
+                    }
+                  );
+                  jQuery('body').append('<form id=\"redirector\" action=\"#{url_hash[:url]}\" method=\"#{(url_hash[:method] || 'get').to_s.upcase}\">' + parts + '</form>'.replace('#{id_replacement}', ary[0])); jQuery('#redirector').submit();
+                "
+              end
+            }
+          }
+        }
+      }
+    ).appendTo('#t_#{@table_id}');
+EOS
     end
   end
 end
