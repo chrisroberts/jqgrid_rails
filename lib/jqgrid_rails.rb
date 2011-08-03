02ad895e082c00b591786f133d18d9b91841b267
Chris Roberts <chrisroberts.code@gmail.com>
2011-08-03 08:58:54 -0700

    Provide js and css expansions for rails 2 applications

--- lib/jqgrid_rails.rb
+++ lib/jqgrid_rails.rb
@@ -5,6 +5,12 @@ if(defined?(Rails))
   if(Rails.version.split('.').first == '3')
     require 'jqgrid_rails/railtie'
   else
+    ActionView::Helpers::AssetTagHelper.register_javascript_expansion(
+      :jqgrid_rails => %w(/jqgrid_rails/javascripts/jqgrid/grid.locale-en.js /jqgrid_rails/javascripts/jqgrid/jquery.jqGrid.min.js)
+    )
+    ActionView::Helpers::AssetTagHelper.register_stylesheet_expansion(
+      :jqgrid_rails => %w(/jqgrid_rails/stylesheets/jqgrid/ui.jqgrid.css)
+    )
     Dir.glob(File.join(File.dirname(__FILE__), 'jqgrid_rails', '*.rb')).each do |file|
       unless(%w(railtie.rb tasks.rb version.rb).find{|skip| file.ends_with?(skip)})
         require file
