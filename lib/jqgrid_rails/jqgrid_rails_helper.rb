d4e4403e771296a91b1babc48cde9bf00db7bf9a
Chris Roberts <chrisroberts.code@gmail.com>
2011-12-21 00:48:08 -0800

    Include javascript helpers

--- lib/jqgrid_rails/jqgrid_rails_helper.rb
+++ lib/jqgrid_rails/jqgrid_rails_helper.rb
@@ -2,6 +2,7 @@ require 'jqgrid_rails/jqgrid_rails_helpers'
 
 module JqGridRails
   class Helper
+    include ActionView::Helpers::JavaScriptHelper
     include JqGridRails::Helpers
     attr_accessor :table_id
     def initialize(dom_id)
